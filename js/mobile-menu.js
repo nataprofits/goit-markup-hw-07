bdb9e4cd2d3ee3e49a069345c2204b5da7e20302
nataprofits <nataprofits@gmail.com>
2023-01-20 13:56:07 +0000

    fist

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,5 +1,5 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-conteiner');
+  const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
